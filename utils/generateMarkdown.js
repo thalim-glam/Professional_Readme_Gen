ef41fe25a8ff3cc0a76f45dacd9717334550ce0a
Tasneem <thalim.glam@gmail.com>
2023-11-20 16:54:52 -0800

    Just updating a few things

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -28,6 +28,7 @@ function generateMarkdown(data) {
   ## Table of Contents
   - [Installation](#installation)
   - [Usage](#usage)
+  - [Credits](#credits)
   - ${renderLicenseLink(data.license)}
   - [Contributing](#contributing)
   - [Tests](#tests)
